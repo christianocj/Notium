abdf64845f82d7a6e9831616fd4bbf1f4524c8c6
christianocj <christianojulio007@gmail.com>
2026-07-07 00:08:10 +0100

    Adiciona o Controller de Categoria

---
+++ KnowledgeHub.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using KnowledgeHub.Application.Abstrations.Services;
+using KnowledgeHub.Application.DTOs.Categories;
+using KnowledgeHub.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace KnowledgeHub.Api.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    [Authorize]
+    [EnableRateLimiting("fixed")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService) => _categoryService = categoryService;
+
+        private Guid CurrentUserId =>
+            Guid.Parse(User.FindFirstValue(JwtRegisteredClaimNames.Sub)!);
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryDto>> Create(CreateCategoryRequest request, CancellationToken ct)
+        {
+            var category = await _categoryService.CreateAsync(CurrentUserId, request, ct);
+            return CreatedAtAction(nameof(GetAll), category);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAll(CancellationToken ct)
+            => Ok(await _categoryService.GetAllAsync(CurrentUserId, ct));
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CategoryDto>> Update(Guid id, UpdateCategoryRequest request, CancellationToken ct)
+            => Ok(await _categoryService.UpdateAsync(CurrentUserId, id, request, ct));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+        {
+            await _categoryService.DeleteAsync(CurrentUserId, id, ct);
+            return NoContent();
+        }
+    }
+}
