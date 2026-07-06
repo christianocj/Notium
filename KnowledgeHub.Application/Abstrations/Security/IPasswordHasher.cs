06ea06aafb9c621c7fa597f5b3995b2d6fb8bbc9
christianocj <christianojulio007@gmail.com>
2026-07-06 23:55:59 +0100

    Adiciona e implementa as abstrações para o hash da password

---
+++ KnowledgeHub.Application/Abstrations/Security/IPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeHub.Application.Abstrations.Security
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string passwordDigitada, string hashArmazenado);
+    }
+}
---
+++ KnowledgeHub.Infra/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KnowledgeHub.Infra.Security
+{
+    public class BCryptPasswordHasherIPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string passwordDigitada, string hashArmazenado)
+        {
+            return BCrypt.Net.BCrypt.Verify(passwordDigitada, hashArmazenado);
+        }
+    }
+}
